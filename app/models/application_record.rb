2a94a9347bbe31a14a5cd3ae31a5d79ca30ab2e1
Ricky Chilcott <ricky@missionmet.com>
2024-11-02 14:28:59 -0400

    Fix warning

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,7 +2,8 @@ class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
   def self.ransackable_attributes(auth_object = nil)
-    super
+    # List only the attributes you want to be searchable
+    column_names & []  # Empty array means no attributes are searchable by default
   end
 
   def self.ransackable_associations(auth_object = nil)
