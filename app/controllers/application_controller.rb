c9b0a5fd055d8240f032c1fe762058fd9dbfeff8
Ricky Chilcott <ricky@missionmet.com>
2025-01-17 16:00:14 -0500

    protect_from_forgery needs to be prepended

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   include CurrentAttributeSetters
   include Pundit::Authorization
 
+  protect_from_forgery with: :exception, prepend: true
   before_action :authenticate_admin_user!
-  protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || root_path
