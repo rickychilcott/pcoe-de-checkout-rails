f6c64c21d1be6a5df1613593530e0cade4542f7b
Ricky Chilcott <ricky@missionmet.com>
2025-01-19 17:59:42 -0500

    Change colors

--- app/components/badge_component.rb
+++ app/components/badge_component.rb
@@ -3,10 +3,11 @@ class BadgeComponent < ApplicationComponent
   prop :count, Integer, reader: :private
   prop :button_variant, BsVariant, reader: :private
   prop :badge_variant, BsVariant, default: BsVariant::Secondary, reader: :private
+  prop :text_variant, BsVariant, default: BsVariant::Dark, reader: :private
 
   def view_template
     span(class: "btn #{button_variant.bg_class} cursor-default") do
-      plain label
+      span(class: text_variant.text_class) { label }
       span(class: "ms-1 badge #{badge_variant.bg_class}") { count }
     end
   end
--- app/components/bs_variant.rb
+++ app/components/bs_variant.rb
@@ -1,10 +1,14 @@
 class BsVariant < Literal::Enum(String)
   prop :bg_class, String
+  prop :text_class, String
 
-  Primary = new("primary", bg_class: "bg-primary")
-  Secondary = new("secondary", bg_class: "bg-secondary")
-  Success = new("success", bg_class: "bg-success")
-  Danger = new("danger", bg_class: "bg-danger")
-  Warning = new("warning", bg_class: "bg-warning")
-  Info = new("info", bg_class: "bg-info")
+  Primary = new("primary", bg_class: "bg-primary", text_class: "text-primary")
+  Secondary = new("secondary", bg_class: "bg-secondary", text_class: "text-secondary")
+  Success = new("success", bg_class: "bg-success", text_class: "text-success")
+  Danger = new("danger", bg_class: "bg-danger", text_class: "text-danger")
+  Warning = new("warning", bg_class: "bg-warning", text_class: "text-warning")
+  Info = new("info", bg_class: "bg-info", text_class: "text-info")
+  White = new("white", bg_class: "bg-white", text_class: "text-white")
+  Light = new("light", bg_class: "bg-light", text_class: "text-light")
+  Dark = new("dark", bg_class: "bg-dark", text_class: "text-dark")
 end
