bbc8ef00e3822301bb512de8780dee5c36f0d9f3
Ricky Chilcott <ricky@missionmet.com>
2025-01-31 00:41:56 -0500

    Autoload lib/patches

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module PcoeDeCheckout
     config.autoload_paths << "#{root}/app/views"
     config.autoload_paths << "#{root}/app/views/layouts"
     config.autoload_paths << "#{root}/app/views/components"
+    config.autoload_paths << "#{root}/lib/patches"
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
