e6ebecae046f7f3128617a81746266f9a788d3bd
Ricky Chilcott <ricky@missionmet.com>
2025-01-31 14:55:06 -0500

    Default to DoNotReply@ohio.edu

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -1,5 +1,4 @@
 class CustomerMailer < ApplicationMailer
-  SMTP_USER = ENV.fetch("SMTP_USER", "coe-projrms-sa@ohio.edu")
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
@@ -8,10 +7,7 @@ class CustomerMailer < ApplicationMailer
   def reminder_email(admin_user:, customer:, body:)
     mail(
       to: email_address_with_name(customer.email, customer.name),
-      reply_to: [
-        email_address_with_name(admin_user.email, admin_user.name),
-        SMTP_USER
-      ],
+      reply_to: email_address_with_name(admin_user.email, admin_user.name),
       subject: "Reminder: #{customer.name}",
       body:
     )
