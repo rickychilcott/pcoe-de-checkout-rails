ccf913de91e7a3b633428fbd6e7888ec77d68e63
Ricky Chilcott <ricky@missionmet.com>
2025-01-31 14:39:32 -0500

    Improve CustomerMailer and preview

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -1,4 +1,5 @@
 class CustomerMailer < ApplicationMailer
+  SMTP_USER = ENV.fetch("SMTP_USER", "coe-projrms-sa@ohio.edu")
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
@@ -7,7 +8,10 @@ class CustomerMailer < ApplicationMailer
   def reminder_email(admin_user:, customer:, body:)
     mail(
       to: email_address_with_name(customer.email, customer.name),
-      from: email_address_with_name(admin_user.email, admin_user.name),
+      reply_to: [
+        email_address_with_name(admin_user.email, admin_user.name),
+        SMTP_USER
+      ],
       subject: "Reminder: #{customer.name}",
       body:
     )
--- spec/mailers/previews/customer_mailer_preview.rb
+++ spec/mailers/previews/customer_mailer_preview.rb
@@ -2,6 +2,10 @@
 class CustomerMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/customer_mailer_mailer/reminder_email
   def reminder_email
-    CustomerMailer.reminder_email
+    CustomerMailer.reminder_email(
+      admin_user: AdminUser.first,
+      customer: Customer.first,
+      body: "This is a reminder email."
+    )
   end
 end
