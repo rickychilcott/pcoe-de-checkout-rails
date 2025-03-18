d993983fc9e24c8df6f7efa76d0adbec01cfa3b3
Ricky Chilcott <ricky@missionmet.com>
2025-03-18 16:20:46 -0400

    Upgrade to latest gems

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", :as => :rails_health_check
 
   constraints(AdminConstraint) do
-    mount Avo::Engine, at: Avo.configuration.root_path
+    mount_avo
 
     resources :customers, only: [:index, :new, :create, :show] do
       resources :checkouts, only: [:create], module: :customers
