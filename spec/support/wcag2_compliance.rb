08e8b936e9b0dc12fb5476802936eab44084537c
Ricky Chilcott <ricky@missionmet.com>
2025-06-06 16:29:41 -0400

    Update gems

--- spec/support/wcag2_compliance.rb
+++ spec/support/wcag2_compliance.rb
@@ -2,7 +2,7 @@ module Wcag2Compliance
   def be_wcag2_accessible
     be_axe_clean
       .according_to(:wcag2a, :wcag2aa)
-      .skipping("duplicate-id", "color-contrast", "aria-required-children", "select-name")
+      .skipping("duplicate-id", "color-contrast", "aria-required-children", "select-name", "aria-allowed-attr")
   end
 end
 
